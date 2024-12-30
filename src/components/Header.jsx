3b0772a427ed28cdaa51cfdda37da5d3fb21f977
Tanapon T. <bossep9@gmail.com>
2024-12-30 20:52:46 +0700

    useAnimate hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
-import NewChallenge from './NewChallenge.jsx';
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import NewChallenge from "./NewChallenge.jsx";
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -16,11 +16,16 @@ export default function Header() {
   return (
     <>
       <AnimatePresence>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
       </AnimatePresence>
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <motion.button whileHover={{scale:1.1}} transition={{type:'spring', stiffness:500}} onClick={handleStartAddNewChallenge} className="button">
+        <motion.button
+          whileHover={{ scale: 1.1, backgroundColor: "#8b11f0" }}
+          transition={{ type: "spring", stiffness: 500 }}
+          onClick={handleStartAddNewChallenge}
+          className="button"
+        >
           Add Challenge
         </motion.button>
       </header>
--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -3,12 +3,14 @@ import { useContext, useRef, useState } from "react";
 import { ChallengesContext } from "../store/challenges-context.jsx";
 import Modal from "./Modal.jsx";
 import images from "../assets/images.js";
-import { motion } from "framer-motion";
+import { motion, useAnimate,stagger } from "framer-motion";
 
 export default function NewChallenge({ onDone }) {
   const title = useRef();
   const description = useRef();
   const deadline = useRef();
+
+  const [scope, animate] = useAnimate();
 
   const [selectedImage, setSelectedImage] = useState(null);
   const { addChallenge } = useContext(ChallengesContext);
@@ -32,6 +34,7 @@ export default function NewChallenge({ onDone }) {
       !challenge.deadline.trim() ||
       !challenge.image
     ) {
+      animate('input, textarea', { x: [-10, 0, 10, 0]}, { type: 'spring', duration: 0.8, delay: stagger(0.2) });
       return;
     }
 
@@ -41,7 +44,7 @@ export default function NewChallenge({ onDone }) {
 
   return (
     <Modal title="New Challenge" onClose={onDone}>
-      <form id="new-challenge" onSubmit={handleSubmit}>
+      <form id="new-challenge" onSubmit={handleSubmit} ref={scope}>
         <p>
           <label htmlFor="title">Title</label>
           <input ref={title} type="text" name="title" id="title" />
@@ -69,7 +72,7 @@ export default function NewChallenge({ onDone }) {
                 hidden: { opacity: 0, scale: 0.5 },
                 visible: {
                   opacity: 1,
-                  scale: 1,
+                  scale: [0.8, 1.3, 1], //scale from 0.8 to 1.3 to 1
                   transition: { type: "spring" },
                 },
               }}
